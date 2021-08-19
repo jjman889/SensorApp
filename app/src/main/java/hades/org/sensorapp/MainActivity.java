caa83e6a4c8c4d975f4bbe71d1af9e9a3fb6f36b
jjman889 <mathzeta@gmail.com>
2021-08-19 23:01:23 +0800

    Update MainActivity.java
    test

--- app/src/main/java/hades/org/sensorapp/MainActivity.java
+++ app/src/main/java/hades/org/sensorapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        compass_bt = (Button) findViewById(R.id.start_compass)
+        compass_bt = (Button) findViewById(R.id.start_compass);
         spirit_bt = (Button) findViewById(R.id.start_spirit);
     }
 
